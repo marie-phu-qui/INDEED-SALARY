31a86e5ef51ec8283ab42bf449ec56f60febf102
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-09-30 17:11:09 +0200

    upload data lists to mongoDB - add bash scripts to backup DB

--- data/mongoDB/mongotest1.py
+++ data/mongoDB/mongotest1.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 from random import randint
 #Step 1: Connect to MongoDB - Note: Change connection string as needed
 client = MongoClient(port=27017)
-# Create collection named "indeed_jobs"
-db=client.indeed_jobs
+# Create collection named "tech_jobs"
+db=client.tech_jobs
 #Step 2: Create sample data
 titre = ['a', 'b', 'c']
 company = ['d', 'e', 'f']
@@ -12,18 +12,18 @@ salary = [1, 2, 3]
 job_description = ['abab', 'blabla', 'laclac']
 date = ['1/a', '2/b', '3/c']
 # generate 500 random data
-for x in range(1, 501):
+for x in range(len(titre)):
     indeed_jobs = {
-        'titre' : titre[randint(0, (len(titre)-1))],
-        'company' : company[randint(0, (len(company)-1))],
-        'adress' : adress[randint(0, (len(adress)-1))],
-        'salary' : randint(0, 5),
-        'job_description' : job_description[randint(0, (len(job_description)-1))],
-        'date' : date[randint(0, (len(date)-1))] 
+        'titre' : titre[x],
+        'company' : company[x],
+        'adress' : adress[x],
+        'salary' : salary[x],
+        'job_description' : job_description[x],
+        'date' : date[x] 
     }
-    #Step 3: Insert business object directly into MongoDB via isnert_one
+    #Step 3: Insert business object directly into MongoDB via insert_one
     result=db.indeed_jobs.insert_one(indeed_jobs)
     #Step 4: Print to the console the ObjectID of the new document
     print('Created {0} of 100 as {1}'.format(x,result.inserted_id))
 #Step 5: Tell us that you are done
-print('finished creating 100 business reviews')
+print('finished uploading collected data to database tech_jobs - collections indeed_jobs')
